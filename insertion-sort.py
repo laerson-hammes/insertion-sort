07175b85699f8991532a74080f3f11ce81567291
Laerson Hammes <laersonha@gmail.com>
2021-03-15 21:07:04 -0300

    Update insertion-sort.py

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,15 +1,15 @@
-def sortList(unordered_list):
+def sort_list(unordered_list):
    for index, _ in enumerate(unordered_list):
       if index + 1 < len(unordered_list):
          if unordered_list[index] > unordered_list[index + 1]:
             nextItem = unordered_list[index + 1]
             unordered_list[index + 1] = unordered_list[index]
             unordered_list[index] = nextItem
-            sortList(unordered_list)
+            sort_lst(unordered_list)
    
    return unordered_list
 
 
 unordered_list = [6, 5, 8, 0, 1, 9, 4, 3, 2, 7]
-end_list = sortList(unordered_list)
+end_list = sort_list(unordered_list)
 print(end_list)
